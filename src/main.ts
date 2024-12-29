7f397d067790b014c13fdf70db13aa0166ad4e25
vogelnahuel <vogelnahuel@gmail.com>
2024-12-29 17:10:02 -0300

    hibrid app

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './AppModule';
 import { CustomLogger } from './Helpers/Middlewares/CustomLogger';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -18,10 +20,30 @@ async function bootstrap() {
     app.useLogger(app.get(CustomLogger));
     app.use(helmet());
 
+    // Configurar el microservicio gRPC
+    app.connectMicroservice<MicroserviceOptions>({
+        transport: Transport.GRPC,
+        options: {
+            package: ['user', 'role', 'permission', 'auth'], // Nombres de los paquetes definidos en tus archivos `.proto`
+            protoPath: [
+                join(__dirname, './Protos/User.proto'), // Ruta relativa al archivo User.proto
+                join(__dirname, './Protos/Role.proto'), // Ruta relativa al archivo Role.proto
+                join(__dirname, './Protos/Permission.proto'), // Ruta relativa al archivo Permission.proto
+                join(__dirname, './Protos/Login.proto'), // Ruta relativa al archivo Login.proto
+            ],
+            url: '0.0.0.0:50051', // Dirección y puerto para gRPC
+        },
+    });
+
     app.use(BodyParser.json());
     app.use(BodyParser.urlencoded({ extended: true }));
     app.setGlobalPrefix('api/v1/auth-seed-ms');
     app.enableCors();
+    await app.startAllMicroservices(); // Inicia el microservicio gRPC
     await app.listen(configService.get<string>('PORT'));
+
+    console.log('Application is running:');
+    console.log('- HTTP: http://localhost:3000');
+    console.log('- gRPC: 0.0.0.0:50051');
 }
 bootstrap();
