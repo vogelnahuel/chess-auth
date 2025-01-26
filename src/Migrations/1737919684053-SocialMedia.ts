a4a1c7d8581f2274bbb740e5faae5d77bcd18f64
vogelnahuel <vogelnahuel@gmail.com>
2025-01-26 16:28:22 -0300

    socialmedia

---
+++ src/Migrations/1737919684053-SocialMedia.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SocialMedia1737919684053 implements MigrationInterface {
+    name = 'SocialMedia1737919684053'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" ADD "isSocialMedia" boolean NOT NULL DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "isSocialMedia"`);
+    }
+
+}
